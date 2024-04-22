3f234e7b60d3af9bdb9b1edde55f995354bcb734
snappiermars <snappiermars848@gmail.com>
2024-04-21 19:15:53 -0600

    Sonido Singelton

--- Assets/Scripts/Moneda.cs
+++ Assets/Scripts/Moneda.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Moneda : MonoBehaviour
 {
@@ -8,20 +9,15 @@ public class Moneda : MonoBehaviour
     [SerializeField] private float cantidadPuntos;
     [SerializeField] private Puntaje puntaje;
 
+    [SerializeField] private AudioClip clip;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")){
+            ControladorSonido.Instance.EjecutarSonido(clip);
             puntaje.SumarPuntos(cantidadPuntos);
             Destroy(gameObject);
+            
         }
     }
-    void Start()
-    {
-        
-    }
-
-    
-    void Update()
-    {
-        
-    }
+   
 }
